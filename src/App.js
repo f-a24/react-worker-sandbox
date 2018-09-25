a2a8647a98bb671c4956dd7c25df222bc86914f5
Atsushi <>
2018-09-25 23:48:38 +0900

    Add color generate

--- src/App.js
+++ src/App.js
@@ -1,56 +1,75 @@
-import React from 'react';
-import styled from 'styled-components';
-import { compose, withStateHandlers, lifecycle } from 'recompose';
-import { SketchPicker } from 'react-color';
-import Worker from './worker.js';
+import React, { Fragment } from "react";
+import styled from "styled-components";
+import { compose, withStateHandlers, lifecycle } from "recompose";
+import { SketchPicker } from "react-color";
+import Worker from "./worker.js";
 
 const worker = new Worker();
 
 export default compose(
   withStateHandlers(
-  {
-    color: {
-      main: '#000',
-      sub1: '#000',
-      sub2: '#000',
-      sub3: '#000',
-      sub4: '#000',
-      sub5: '#000'
+    {
+      color: {
+        main: "#000",
+        sub: {
+          "1": {},
+          "2": {},
+          "3": {},
+          "4": {},
+          "5": {},
+          "6": {},
+          "7": {}
+        }
+      }
+    },
+    {
+      colorHandle: ({ color }) => v => ({ color: Object.assign({}, color, v) })
     }
-  },
-  {
-    colorHandle: ({ color }) => v => ({ color: Object.assign({}, color, v)})
-  }),
+  ),
   lifecycle({
     componentDidMount() {
-      worker.addEventListener('message', message => {
-        this.props.colorHandle({ main: message.data });
+      worker.addEventListener("message", message => {
+        this.props.colorHandle({
+          sub: Object.assign({}, this.props.color.sub, message.data)
+        });
       });
     },
     componentDidUnmount() {
       worker.terminate();
     }
   })
-  )(({
-    color,
-    colorHandle
-  }) => (
-  <div>
-    <StyledSketch color={color.main} onChange={c => {
-      worker.postMessage(c.rgb);
-    }} />
-    {Object.keys(color).map(k => <Panel key={k} color={color[k]} />)}
-  </div>
+)(({ color, colorHandle }) => (
+  <Area>
+    <SketchPicker
+      color={color.main}
+      onChange={c => {
+        colorHandle({ main: c.hex });
+        Object.keys(color.sub).forEach(n => {
+          worker.postMessage({ n: n, main: c.hex });
+        });
+      }}
+    />
+    <div>
+      {Object.keys(color.sub).map(n => (
+        <div key={n}>
+          <Panel color={color.main} />
+          {Object.keys(color.sub[n]).map(k => (
+            <Panel key={k} color={color.sub[n][k]} />
+          ))}
+        </div>
+      ))}
+    </div>
+  </Area>
 ));
 
-const StyledSketch = styled(SketchPicker)`
-  display: inline-block;
+const Area = styled.div`
+  display: flex;
 `;
 
 const Panel = styled.div`
   display: inline-block;
-  width: 100px;
-  height: 100px;
-  margin: 1rem;
-  background-color: ${({color}) => color};
+  width: 50px;
+  height: 50px;
+  margin: 0.5rem;
+  background-color: ${({ color }) => color || "#000"};
 `;
--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,121 @@
-self.addEventListener('message', message => {
-	setTimeout(() => {
-		const rgb = message.data;
-		self.postMessage(`rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, ${rgb.a});`);
-	}, 500);
+import luma from "./lib/luma";
+import contrast from "./lib/contrast";
+import mix from "./lib/mix";
+import shade from "./lib/shade";
+import spin from "./lib/spin";
+import tint from "./lib/tint";
+
+const genarate = (n, main) => {
+  switch (n) {
+    // モノクロ
+    case "1":
+      return {
+        sub1: main
+          ? contrast(contrast(main), tint(main, 48), shade(main, 24))
+          : "",
+        sub2: main ? contrast(main, tint(main, 18), shade(main, 16)) : "",
+        sub3: main ? contrast(main, tint(main, 36), shade(main, 32)) : "",
+        sub4: main ? contrast(main, tint(main, 72), shade(main, 64)) : "",
+        sub5: main
+          ? `rgb(${luma(main) * 100}%,${luma(main) * 100}%,${luma(main) *
+              100}%)`
+          : ""
+      };
+    // 類似色
+    case "2":
+      return {
+        sub1: main ? contrast(main, tint(main, 52), shade(main, 32)) : "",
+        sub2: main ? spin(main, 30) : "",
+        sub3: main
+          ? contrast(main, tint(spin(main, 30), 52), shade(spin(main, 30), 32))
+          : "",
+        sub4: main ? spin(main, -30) : "",
+        sub5: main
+          ? contrast(
+              main,
+              tint(spin(main, -30), 52),
+              shade(spin(main, -30), 32)
+            )
+          : ""
+      };
+    // 補色
+    case "3":
+      return {
+        sub1: main ? tint(main, 68) : "",
+        sub2: main ? shade(main, 48) : "",
+        sub3: main ? spin(main, 180) : "",
+        sub4: main ? tint(spin(main, 180), 68) : "",
+        sub5: main ? shade(spin(main, 180), 48) : ""
+      };
+    // トライアド
+    case "4":
+      return {
+        sub1: main ? contrast(main, tint(main, 52), shade(main, 32)) : "",
+        sub2: main ? spin(main, 120) : "",
+        sub3: main
+          ? contrast(
+              main,
+              tint(spin(main, 120), 52),
+              shade(spin(main, 120), 32)
+            )
+          : "",
+        sub4: main ? spin(main, -120) : "",
+        sub5: main
+          ? contrast(
+              main,
+              tint(spin(main, -120), 52),
+              shade(spin(main, -120), 32)
+            )
+          : ""
+      };
+    // 分裂補色
+    case "5":
+      return {
+        sub1: main ? contrast(main, tint(main, 52), shade(main, 32)) : "",
+        sub2: main ? spin(main, 180 + 30) : "",
+        sub3: main
+          ? contrast(
+              main,
+              tint(spin(main, 180 + 30), 52),
+              shade(spin(main, 180 + 30), 32)
+            )
+          : "",
+        sub4: main ? spin(main, 180 - 30) : "",
+        sub5: main
+          ? contrast(
+              main,
+              tint(spin(main, 180 - 30), 52),
+              shade(spin(main, 180 - 30), 32)
+            )
+          : ""
+      };
+    // 補色を混色
+    case "6":
+      return {
+        sub1: main ? mix(main, spin(main, 180), 80) : "",
+        sub2: main ? mix(main, spin(main, 180), 60) : "",
+        sub3: main ? mix(main, spin(main, 180), 40) : "",
+        sub4: main ? mix(main, spin(main, 180), 20) : "",
+        sub5: main ? spin(main, 180) : ""
+      };
+    // 類似色（左右180°）
+    case "7":
+      return {
+        sub1: main ? spin(main, 60) : "",
+        sub2: main ? spin(main, 30) : "",
+        sub3: main ? spin(main, -30) : "",
+        sub4: main ? spin(main, -60) : "",
+        sub5: main
+          ? `rgb(${luma(main) * 100}%,${luma(main) * 100}%,${luma(main) *
+              100}%)`
+          : ""
+      };
+    default:
+      return {};
+  }
+};
+
+self.addEventListener("message", message => {
+  const { n, main } = message.data;
+  self.postMessage({ [n]: genarate(n, main) });
 });
